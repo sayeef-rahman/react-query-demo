a6c1fe0f3065916951e8418e51ab42591069230f
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-19 23:15:11 +0600

    traditional fetching & visualixation done

--- src/components/SuperHeroPage.js
+++ src/components/SuperHeroPage.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 const SuperHeroPage = () => {
   const [isLoadin, setIsLoading] = useState(true);
-  const [data, setData] = useState();
+  const [products, setProducts] = useState();
 
   useEffect(() => {
     axios.get("http://localhost:4000/products").then((res) => {
-        setData(res);
-      setIsLoading(false);
+      if (res.status === 200) {
+        setProducts(res?.data);
+        setIsLoading(false);
+      } else {
+        alert("server error");
+        setProducts(undefined);
+      }
     });
   }, []);
+  console.log(products);
   return (
     <div className="container">
-      <h1 className="text-center mt-5">Tradition Data Fetching</h1>
+      <h1 className="text-center mt-5">Tradition Data Fetching (Products)</h1>
+      {products?.map((product) => {
+        const {id, title, price} = product;
+        return <div key={id} className="my-2">{id} - {title} [price - {price} BDT]</div>
+      })}
     </div>
   );
 };
