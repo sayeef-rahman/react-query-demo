501857fae7b2e2b8b76c3f3ebf50b534140ac28c
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-19 20:42:49 +0600

    components added

---
+++ src/components/RQsuperHeroPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RQsuperHeroPage = () => {
+    return (
+        <div>
+            <h1>React Query data fetching</h1>
+        </div>
+    );
+};
+
+export default RQsuperHeroPage;
---
+++ src/components/SuperHeroPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuperHeroPage = () => {
+    return (
+        <div>
+            <h1>Tradition Data Fetching</h1>
+        </div>
+    );
+};
+
+export default SuperHeroPage;
