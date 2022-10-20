779188cf0d5b10d931ce281934ad5ccc43a1041d
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-20 09:36:54 +0600

    classical product error handled

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -3,28 +3,39 @@ import axios from "axios";
 const ProductsPage = () => {
   const [isLoadin, setIsLoading] = useState(true);
   const [products, setProducts] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
-    axios.get("http://localhost:4000/products").then((res) => {
-      if (res.status === 200) {
-        setProducts(res?.data);
+    axios
+      .get("http://localhost:4000/products")
+      .then((res) => {
+        if (res.status === 200) {
+          setProducts(res?.data);
+          setIsLoading(false);
+        } else {
+          alert("server error");
+          setProducts(undefined);
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
         setIsLoading(false);
-      } else {
-        alert("server error");
-        setProducts(undefined);
-      }
-    });
+      });
   }, []);
-  
-  if(isLoadin){
-    return <h1 className="text-center mt-5"> Loading.....</h1>
+
+  if (isLoadin) {
+    return <h1 className="text-center mt-5"> Loading.....</h1>;
   }
   return (
     <div className="container">
       <h1 className="text-center mt-5">Tradition Data Fetching (Products)</h1>
       {products?.map((product) => {
-        const {id, title, price} = product;
-        return <div key={id} className="my-2">{id} - {title} [price - {price} BDT]</div>
+        const { id, title, price } = product;
+        return (
+          <div key={id} className="my-2">
+            {id} - {title} [price - {price} BDT]
+          </div>
+        );
       })}
     </div>
   );
